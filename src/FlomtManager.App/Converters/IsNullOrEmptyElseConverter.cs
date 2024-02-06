634df5300b16136a8d1d692527e2bbab091ad9f9
Artjom Strelkov <artoxaaa5@gmail.com>
2024-02-07 01:40:58 +0200

    change string converter

--- src/FlomtManager.App/Converters/IsNullOrEmptyElseConverter.cs
+++ src/FlomtManager.App/Converters/IsNullOrEmptyElseConverter.cs
@@ -4,16 +4,16 @@ using System.Globalization;
 
 namespace FlomtManager.App.Converters
 {
-    public class IsNullOrEmptyDashConverter : IValueConverter
+    public class IsNullOrEmptyElseConverter : IValueConverter
     {
         public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-            return string.IsNullOrEmpty((string?)value) ? "-" : value;
+            return string.IsNullOrEmpty((string?)value) ? parameter : value;
         }
 
         public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-            return (string?)value == "-" ? string.Empty : value;
+            return (string?)value == (string?)parameter ? string.Empty : value;
         }
     }
 }
